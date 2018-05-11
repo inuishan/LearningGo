be4cf25800699f296d0aec7f24fd4b266db16da0
Ishan <ishanjain1991@gmail.com>
2018-05-11 10:14:33 +0530

    adding seed to rand. The seed is strong on this one.

--- pkg/hello_world.go
+++ pkg/hello_world.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	rand2 "math/rand"
+	"time"
 )
 
 func main()  {
+	rand2.Seed( time.Now().UTC().UnixNano())
 	fmt.Println("Hello")
 	rand()
 }
 
 func rand() {
-	fmt.Println("A random number ", rand2.Intn(7))
+	fmt.Println("A random number ", rand2.Intn(10))
 }
