ddd54df6d7ef865688f1c7b4972c5844cf226b6a
Ishan <ishanjain1991@gmail.com>
2018-05-11 10:23:19 +0530

    sqrt function

--- pkg/hello_world.go
+++ pkg/hello_world.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	rand2 "math/rand"
 	"time"
+	"math"
 )
 
 func main()  {
 	rand2.Seed( time.Now().UTC().UnixNano())
 	fmt.Println("Hello")
-	rand()
+	sqrt()
 }
 
-func rand() {
-	fmt.Println("A random number ", rand2.Intn(10))
+func rand() int {
+	return rand2.Intn(10)
+}
+
+func sqrt() {
+	fmt.Printf("Square root of a random number is %g", math.Sqrt(float64(rand())))
 }
