ae68e229eb87dd1ee56d34e9843eef892b57f9ea
Ishan <ishanjain1991@gmail.com>
2018-05-11 10:11:42 +0530

    random number

--- pkg/hello_world.go
+++ pkg/hello_world.go
@@ -1,7 +1,15 @@
-package pkg
+package main
 
-import "fmt"
+import (
+	"fmt"
+	rand2 "math/rand"
+)
 
 func main()  {
 	fmt.Println("Hello")
+	rand()
+}
+
+func rand() {
+	fmt.Println("A random number {}", rand2.Intn(7))
 }
